1385f43d6814afb735dd5a841618156e50008533
rob1997 <robelgetnetrg@gmail.com>
2022-04-27 16:51:23 +0300

    Reset - Reset project to empty to ready for package restructuring

--- Assets/Scripts/Core/Game/Manager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Manager : MonoBehaviour
-{
-    public abstract void Initialize();
-}
